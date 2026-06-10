95cbf6d1a731a47ad40b9f1c028587f6b571f39d
priyansh0602 <priyanshheeranandani2@gmail.com>
2026-06-11 01:40:59 +0530

    feat(mobile): implement email inbox and conversation screens

---
+++ apps/mobile/src/screens/email/index.ts
@@ -0,0 +1,3 @@
+export { default as EmailScreen } from "./EmailScreen";
+export { default as ConversationListScreen } from "./ConversationListScreen";
+export { default as ConversationViewScreen } from "./ConversationViewScreen";
